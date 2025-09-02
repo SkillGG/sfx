4a777b3bba54fc2c27b1d278ac528b97677d21b2
SkillGG <skilletssgames@gmail.com>
2025-09-02 03:29:01 +0200

    Add tests/code for string parse single and hide

--- src/app/_components/sfx/parse.test.tsx
+++ src/app/_components/sfx/parse.test.tsx
@@ -8,7 +8,12 @@ const emptyFieldData = {
   read: "",
 };
 
-const emptyFieldResult = { read: [], def: [], extra: [] };
+const emptyFieldResult: SFXFieldsData = {
+  read: [],
+  def: [],
+  extra: [],
+  tlExtra: [],
+};
 
 it("String parse - empty", () => {
   expect(
@@ -27,13 +32,92 @@ describe("String parse - singular", () => {
     { ...emptyFieldData, extra: "testV", key: "extra" },
     { ...emptyFieldData, read: "testV", key: "read" },
   ])(`$key`, (val) => {
-    expect(parseSFXFields(val)).toEqual({
+    expect(parseSFXFields(val)).toEqual<SFXFieldsData>({
       ...emptyFieldResult,
-      [val.key]: [{ type: "string", value: "testV", index: 1 }],
+      [val.key]: [{ type: "string", value: "testV", index: 1, hidden: false }],
     });
   });
 });
 
 describe("String parse - multiple", () => {
-  it.each([]);
+  it(`every`, () => {
+    expect(
+      parseSFXFields({
+        ...emptyFieldData,
+        def: "testDef",
+        extra: "testExtra",
+        read: "testRead",
+      }),
+    ).toEqual<SFXFieldsData>({
+      ...emptyFieldResult,
+      read: [{ type: "string", value: "testRead", index: 1, hidden: false }],
+      def: [{ type: "string", value: "testDef", index: 2, hidden: false }],
+      extra: [{ type: "string", value: "testExtra", index: 3, hidden: false }],
+    });
+  });
+});
+
+describe("String parse - hide", () => {
+  it.each(["def", "read", "extra"])(`%s`, (q) => {
+    expect(
+      parseSFXFields({ ...emptyFieldData, [q]: `a;b`, tlExtra: `-${q}(1)` }),
+    ).toEqual<SFXFieldsData>({
+      ...emptyFieldResult,
+      [q]: [
+        { type: "string", hidden: true, index: 1, value: "a" },
+        { type: "string", hidden: false, index: 2, value: "b" },
+      ],
+    });
+  });
+});
+
+describe("String parse - jump", () => {
+  it.each([
+    { from: "read", to: "read" } as const,
+    { from: "read", to: "extra" } as const,
+    { from: "read", to: "def" } as const,
+    { from: "def", to: "read" } as const,
+    { from: "def", to: "def" } as const,
+    { from: "def", to: "extra" } as const,
+    { from: "extra", to: "read" } as const,
+    { from: "extra", to: "def" } as const,
+    { from: "extra", to: "extra" } as const,
+  ])("$from => $to", ({ from, to }) => {
+    const failObj: Parameters<typeof parseSFXFields>[0] = {
+      ...emptyFieldData,
+      [to]: `[_${to}(1)]a;b`,
+    };
+    const successObj: Parameters<typeof parseSFXFields>[0] = {
+      ...emptyFieldData,
+      [from]: `[_${to}(1)]a`,
+      [to]: `b`,
+    };
+
+    expect(
+      parseSFXFields(from === to ? failObj : successObj),
+    ).toEqual<SFXFieldsData>(
+      from === to
+        ? {
+            ...emptyFieldResult,
+            // fail
+            [to]: [
+              { hidden: false, type: "string", value: "b", index: 2 },
+              {
+                hidden: false,
+                type: "string",
+                value: `[_${to}(1)]a`,
+                index: 1,
+              },
+            ],
+          }
+        : {
+            ...emptyFieldResult,
+            [from]: [],
+            [to]: [
+              { hidden: false, type: "string", value: "b", index: 1 },
+              { hidden: false, type: "string", value: "a", index: 1.5 },
+            ],
+          },
+    );
+  });
 });
--- src/app/_components/sfx/parse.tsx
+++ src/app/_components/sfx/parse.tsx
@@ -1,6 +1,6 @@
 import { type CollapsedOnomatopoeia } from "@/utils";
 
-type FieldBase = { index: number };
+type FieldBase = { index: number; hidden: boolean };
 
 export type SFXField = FieldBase &
   (
@@ -20,8 +20,63 @@ export type SFXFieldsData = {
   read?: SFXField[];
 };
 
+type RemoveLineData = {
+  index: number;
+  key: keyof SFXFieldsData;
+};
+
+type JumpLineData = {
+  index: number;
+  to: keyof SFXFieldsData;
+};
+
+export const stringToSFXFieldKey = (k: string): keyof SFXFieldsData | null => {
+  switch (k) {
+    case "d":
+    case "def":
+    case "definition":
+      return "def";
+    case "e":
+    case "extra":
+    case "ex":
+      return "extra";
+    case "r":
+    case "read":
+      return "read";
+  }
+  return null;
+};
+
+export const parseLineAsJump = (str: string): JumpLineData | null => {
+  const rx = /^\[_(?<to>[a-z]+)\((?<index>\d+)\)\]$/.exec(str);
+  if (!rx) return null;
+  const toStr = rx.groups?.to;
+  if (!toStr) return null;
+  const to = stringToSFXFieldKey(toStr);
+  if (!to) return null;
+  const idx = rx.groups?.index;
+  if (!idx) return null;
+  const index = Number(idx);
+  if (isNaN(index) || index < 0 || !isFinite(index)) return null;
+  return { index, to };
+};
+
+export const parseRemoveLine = (str: string): RemoveLineData | null => {
+  const rx = /^\-(?<key>[a-z]+)\((?<index>\d+)\)$/i.exec(str);
+  if (!rx) return null;
+  const key = rx.groups?.key;
+  if (!key) return null;
+  const index = rx.groups?.index;
+  if (!index) return null;
+  const i = Number(index);
+  if (i <= 0 || !isFinite(i) || isNaN(i)) return null;
+  const kKey = stringToSFXFieldKey(key);
+  if (!kKey) return null;
+  return { index: i, key: kKey };
+};
+
 export const parseLineAsSFXField = (str: string): SFXField => {
-  return { type: "string", value: str, index: -1 };
+  return { type: "string", value: str, index: -1, hidden: false };
 };
 
 export const parseSFXFields = (
@@ -31,29 +86,69 @@ export const parseSFXFields = (
 ): SFXFieldsData => {
   const fieldsData: SFXFieldsData = {};
 
+  let index = 0;
+
+  const specialIndices: {
+    key: keyof SFXFieldsData;
+    index: number;
+    newIndex: number;
+  }[] = [];
+
+  fieldsData.read =
+    data.read
+      ?.split(";")
+      .map<SFXField | null>((line) => {
+        if (!line) return null;
+
+        const jump = parseLineAsJump(line);
+
+        if (jump) {
+          // TODO: figure this out
+          return null;
+        }
+
+        const parsed = parseLineAsSFXField(line);
+        return { ...parsed, index: parsed.index < 0 ? ++index : parsed.index };
+      })
+      .filter((q) => !!q) ?? [];
+
   fieldsData.def = data.def
     .split(";")
-    .map<SFXField | null>((line, i) => {
+    .map<SFXField | null>((line) => {
       if (!line) return null;
-      return { ...parseLineAsSFXField(line), index: i + 1 };
+      const parsed = parseLineAsSFXField(line);
+      return { ...parsed, index: parsed.index < 0 ? ++index : parsed.index };
     })
     .filter((q) => !!q);
 
   fieldsData.extra =
     data.extra
       ?.split(";")
-      .map<SFXField | null>((line, i) => {
+      .map<SFXField | null>((line) => {
         if (!line) return null;
-        return { ...parseLineAsSFXField(line), index: i + 1 };
+        const parsed = parseLineAsSFXField(line);
+        return { ...parsed, index: parsed.index < 0 ? ++index : parsed.index };
       })
       .filter((q) => !!q) ?? [];
 
-  fieldsData.read =
-    data.read
+  fieldsData.tlExtra =
+    data.tlExtra
       ?.split(";")
-      .map<SFXField | null>((line, i) => {
+      .map<SFXField | null>((line) => {
         if (!line) return null;
-        return { ...parseLineAsSFXField(line), index: i + 1 };
+        const isRemoveLine = parseRemoveLine(line);
+        if (isRemoveLine) {
+          const data = fieldsData[isRemoveLine.key]?.find(
+            (_, i) => i + 1 === isRemoveLine.index,
+          );
+          if (data) data.hidden = true;
+          return null;
+        }
+        const parsed = parseLineAsSFXField(line);
+        return {
+          ...parsed,
+          index: parsed.index < 0 ? ++index : parsed.index,
+        };
       })
       .filter((q) => !!q) ?? [];
 
